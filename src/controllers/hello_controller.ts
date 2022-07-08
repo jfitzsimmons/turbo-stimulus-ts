8c1d39172e9c0d68c0675dd02e68bf8ca111a509
jfitzsimmons <jfitzcode@gmail.com>
2022-07-08 08:06:18 -0500

    routing, dark theme, step work

--- src/controllers/hello_controller.ts
+++ src/controllers/hello_controller.ts
@@ -1,20 +1,30 @@
 import { Controller } from "@hotwired/stimulus";
 import * as Turbo from "@hotwired/turbo";
 
+const halves: [number,number][] = [];
+let steps: number[] = [];
 
 
 export default class extends Controller {
   static targets = [ "name", "output", "steps" ];
-  declare readonly halves: [number,number][];
+
   declare readonly nameTarget: HTMLInputElement;
   declare readonly hasOutputTarget: boolean;
   declare readonly outputTarget: Element;
   declare readonly outputTargets: Element[];
   declare readonly stepsTarget: Element;
+  /**
+  steps() {
 
-
+    this.stepsTarget.append(`<div>stepE: ${stepE}</div>`);
+  }
+ */
   greet() {
-  console.log(this.karatsuba(2531, 1467))
+  //console.log(this.karatsuba(2531, 11467))
+  //console.log(this.karatsuba(2531, 1467))
+  console.log(this.karatsuba(25, 63))
+  console.dir(halves)
+  console.dir(steps)
   this.outputTarget.textContent = `Hello, ${this.name}!`;
   }
 
@@ -23,15 +33,21 @@ export default class extends Controller {
     const half2: number = parseInt(whole.substring(divider));
     let arr: [number, number] = [half1,half2]; 
 
-    console.log(`half1: ${half1} half2: ${half2}`);
-    this.halves.push([half1, half2])
+    //console.log(`half1: ${half1} half2: ${half2}`);
+    if (half1 > 9 && half2 > 9) halves.push(arr);
     return arr
 };
 
  karatsuba(num1, num2) {
     if (num1 < 10 || num2 < 10) {
+      /**
+       * testjpf
+       * use singles to show nested low level steps
+       */
+      console.log(`single: ${num1} | ${num2}`);
         return num1 * num2 
     }
+    console.log(`SPLIT: ${num1} | ${num2}`);
     
     const short = Math.min(num1.toString().length, num2.toString().length)
     const  mid = Math.floor(short / 2) 
@@ -39,13 +55,13 @@ export default class extends Controller {
     const [n2h1, n2h2] = this.splitter(num2.toString(), num2.toString().length-mid)
     //TESTJPF feel mlike I'll have to dynamically come up with step vars
     //Steps may vary on greater input values
+    const stepB = this.karatsuba(n1h1, n2h1)
     const stepC = this.karatsuba(n1h2, n2h2)
     const stepE = this.karatsuba(n1h2 + n1h1, n2h2 + n2h1)
-    const stepB = this.karatsuba(n1h1, n2h1)
     //TESTJPF need sme conditional for the values you want to actually store. 
     // multi digit calculations probably
-    console.log(`stepC: ${stepC} stepE: ${stepE} stepB: ${stepB}`);
-    this.stepsTarget.append(`<div>stepE: ${stepE}</div>`);
+    console.log(`stepB: ${stepB} | stepC: ${stepC} |  stepE: ${stepE}`);
+    steps=[stepB,stepC,stepE]
     return (stepB *  10 ** (2 * mid )) + ((stepE - stepB - stepC) * 10 ** mid) + stepC
 } 
   
--- src/controllers/index_controller.ts
+++ src/controllers/index_controller.ts
@@ -8,6 +8,6 @@ export default class extends Controller {
   declare readonly hasOutputTarget: boolean;
 
   jump() {
-    Turbo.visit("/hello.html");
+    Turbo.visit("/hello.html", { action: "replace" });
   }
 }
