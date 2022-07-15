4d86880a17665e69f0f62d127732a004ac43e9d5
jfitzsimmons <jfitzcode@gmail.com>
2022-07-15 07:19:53 -0500

    generic ui errors, result edge cases, more utils

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -1,2 +1,21 @@
 export const isSingle = (n1: number, n2: number) =>
   n1 < 10 || n2 < 10 ? true : false;
+
+export const trailingZeros = (n: number) => {
+  let zeros = "";
+  for (let i = n; i--; ) {
+    zeros += "0";
+  }
+  return zeros;
+};
+
+export const splitter = (whole: string, divider: number) => {
+  const half1: number = parseInt(whole.substring(0, whole.length - divider));
+  const half2: number = parseInt(whole.substring(whole.length - divider));
+  const arr: [number, number] = [half1, half2];
+
+  return arr;
+};
+
+export const getDigit = (num: number, divider: number) =>
+  +(num + "").slice(divider);
